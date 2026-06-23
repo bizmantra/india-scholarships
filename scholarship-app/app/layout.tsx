9817d32c1b2cb4448fd90256725da687cfc27453
Roshan Kumar <roshankumar@Roshans-MacBook-Air.local>
2026-06-23 23:20:41 +0530

    Hardcode Google Analytics ID G-DE8V5ECQZX in layout.tsx

--- scholarship-app/app/layout.tsx
+++ scholarship-app/app/layout.tsx
@@ -43,22 +43,18 @@ export default function RootLayout({
         className={`${inter.variable} ${lora.variable} font-sans antialiased`}
       >
         {children}
-        {process.env.NEXT_PUBLIC_GA_ID && (
-          <>
-            <Script
-              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-              strategy="afterInteractive"
-            />
-            <Script id="google-analytics" strategy="afterInteractive">
-              {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
-              `}
-            </Script>
-          </>
-        )}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-DE8V5ECQZX"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-DE8V5ECQZX');
+          `}
+        </Script>
       </body>
     </html>
   );
